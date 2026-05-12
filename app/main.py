f56a3d27accd8c0b44f4c825138c812d9ec47022
Viraj <hemanthaviraj6@gmail.com>
2026-05-12 22:28:42 +0530

    nnn

--- app/main.py
+++ app/main.py
@@ -42,6 +42,3 @@ async def root():
     return FileResponse(str(STATIC_DIR / "index.html"))
 
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
